8bca6f978bd97626e1105385de71620b9f82644e
Jack <2351733045@qq.com>
2024-11-10 12:10:22 +0800

    第三次提交git，尝试更新获取

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("第一次提交git");
-        System.out.println("第二词提交git，尝试回退");
+        System.out.println("第一次提交git，制造冲突");
+        System.out.println("第二次提交git，尝试回退");
+        System.out.println("第三次提交git，尝试更新获取");
     }
 }
