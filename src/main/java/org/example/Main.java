9d580ffc7b62277740129af52b67469ba1ae5dbe
Jack <2351733045@qq.com>
2024-11-10 11:35:50 +0800

    第二次命令行提交git

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("第一次提交git");
+        System.out.println("第二词提交git，尝试回退");
     }
 }
