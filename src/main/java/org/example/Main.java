176ba524d436398d362c58dda7f1d92a5e8c625e
Jack <2351733045@qq.com>
2024-11-10 12:22:44 +0800

    第四次提交git，尝试冲突

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("第一次提交git，制造冲突");
         System.out.println("第二次提交git，尝试回退");
-        System.out.println("第三次提交git，尝试更新获取");
+        System.out.println("第四次提交git，尝试冲突");
     }
 }
